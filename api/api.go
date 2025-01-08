ec17937358ccdefbe4026459956ee0a881e6b371
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2025-01-08 17:47:05 +0700

    Change cors option to default

--- api/api.go
+++ api/api.go
@@ -64,12 +64,13 @@ func (api *API) Handler() *http.ServeMux {
 
 func (api *API) Start() {
 	// Enable CORS
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-	}).Handler(api.Handler())
+	// corsHandler := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"http://localhost:8100"},
+	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowedHeaders:   []string{"Content-Type", "Authorization"},
+	// 	AllowCredentials: true,
+	// }).Handler(api.Handler())
+	corsHandler := cors.Default().Handler(api.Handler())
 
 	fmt.Println("starting web server at http://localhost:8080")
 	http.ListenAndServe(":8080", corsHandler)
